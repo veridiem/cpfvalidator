6adf3b80b76abf4e7cb045b6b8ce3b7a3af51b9f
veridiem <jessicavga@gmail.com>
2019-05-07 17:04:30 -0300

    Testes implementados, readme versão final

--- test/index.spec.js
+++ test/index.spec.js
@@ -4,16 +4,21 @@ const cpfValidator = require("../lib");
 
 describe("cpfvalidator lib", () => {
     describe("cpfvalidate", () => {
-        it("test1", () =>{
-            expect(cpfValidator("08557720696")).to.equal(true);
+        it("Test1 should return true", () =>{
+            expect(cpfValidator("87351732035")).to.equal(true);
         });
-        it("test2", () =>{
+        it("Test2 should return false", () =>{
             expect(cpfValidator("08557720699")).to.equal(false);
         });
-        it("test3"), () => {
-            
-        }
-
+        it("Test3 should return true", () => {
+            expect(cpfValidator("52660038040")).to.equal(true);
+        });
+        it("Test4 empty space should return false", () => {
+            expect(cpfValidator("")).to.equal(false);
+        })
+        it("Test5 insuficient numbers should return false", () => {
+            expect(cpfValidator("526600380")).to.equal(false);
+        })
     })
 })
 
