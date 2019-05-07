ef309669f0ca61bc742ce9d08534b5ded16f4f10
veridiem <jessicavga@gmail.com>
2019-05-07 16:49:16 -0300

    ReadMe e Eslint na última versão

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,101 @@
 module.exports = {
     'env': {
-        'browser': true,
-        'commonjs': true,
-        'es6': true
+      'browser': true,
+      'commonjs': true,
+      'es6': true
     },
     'extends': 'eslint:recommended',
     'globals': {
-        'Atomics': 'readonly',
-        'SharedArrayBuffer': 'readonly'
+      'Atomics': 'readonly',
+      'SharedArrayBuffer': 'readonly'
     },
     'parserOptions': {
-        'ecmaVersion': 2018
+      'ecmaVersion': 2018
     },
     'rules': {
-        'error':[
+      'indent': [
+        'error',
         2
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'always'
-        ],
-        'id-length':[
-            'error',
-            2
-        ],
-       
+      ],
+      'linebreak-style': [
+        'error',
+        'unix'
+      ],
+      'quotes': [
+        'error',
+        'single'
+      ],
+      'semi': [
+        'error',
+        'always'
+      ],
+      'id-length': 2,
+      'camelcase': ["error", { properties: "always" }],
+      'keyword-spacing': [
+        'error', {
+          'after': true
+        }
+      ],
+      'space-infix-ops': [
+        'error', {
+          'int32Hint': false
+        }
+      ],
+      'comma-spacing': [
+        'error', {
+          'before': false,
+          'after': true
+        }
+      ],
+      'space-before-blocks': 2,
+      'spaced-comment': [
+        'error',
+        'always'
+      ],
+      'key-spacing': [
+        'error', {
+          'beforeColon': true
+        }
+      ],
+      'no-multi-spaces': [
+        'error', {
+          'ignoreEOLComments': false
+        }
+      ],
+      'space-in-parens': [
+        'error',
+        'never'
+      ],
+      'space-before-function-paren': [
+        'error',
+        'never'
+      ],
+      'func-call-spacing': [
+        'error',
+        'never'
+      ],
+      'no-multiple-empty-lines': [
+        'error', {
+          'max': 1,
+          'maxBOF': 1
+        }
+      ],
+      'padded-blocks': [
+        'error',
+        'never'
+      ],
+      'quotes': [
+        'error',
+        'single'
+      ],
+      'no-floating-decimal': 2,
+      'object-property-newline': 2,
+      'brace-style': 2,
+      'eqeqeq': [
+        'error',
+        'always'
+      ],
+      'no-array-constructor': 2,
     }
-};
+  };
+  
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# cpfvalidator
-My exercise at Laboratoria to learn about library. 
+# Validador de CPF - Cadastro de Pessoa Física v.1.0.0
+
+**Esta biblioteca se destina à validação de números de CPF.**
+Na versão atual é capaz de receber o valor de um CPF e informar se o mesmo é válido ou não.
+
+## Como instalar:
+
+```shell
+
+$  npm install cpfveridiem
+
+```
+
+## Como utilizar:
+
+```node
+
+> const cpfValidator = require("cpfveridiem");
+> console.log(cpfValidator (47054783042))
+> // returns "true"
+
+```
+
+## roadmap oficial do projeto
+
+#### versão 1.0.0 (released)
+- funcionalidades: verificar se um número de um CPF é verdadeiro ou falso.
